45e8a3adce7a4635c70db9b3e9071a05e1bc32ee
Tom de Geus <tom@geus.me>
2021-05-20 15:15:06 +0200

    Debugging

--- recipe/install_python.sh
+++ recipe/install_python.sh
@@ -1,1 +1,20 @@
+mkdir build
+cd build
+
+cmake \
+  -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
+  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DPYTHON_EXECUTABLE="$PYTHON" \
+  -DBUILD_PYTHON=1 \
+  ..
+
+make
+
+cd ..
+rm -r build
+
+
+
 python -m pip install . -vv
