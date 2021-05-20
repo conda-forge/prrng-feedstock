2a88bb0d29be9ae14a780a000dc4d4e17f6b609a
Tom de Geus <tom@geus.me>
2021-05-20 15:30:04 +0200

    debugging

--- recipe/install_python.sh
+++ recipe/install_python.sh
@@ -1,3 +1,7 @@
+if [ -d build ]; then
+  rm -r build
+fi
+
 mkdir build
 cd build
 
@@ -15,6 +19,26 @@ make
 cd ..
 rm -r build
 
+# ----
 
+mkdir build
+cd build
+
+cmake \
+  ${CMAKE_ARGS} \
+  -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
+  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DPYTHON_EXECUTABLE="$PYTHON" \
+  -DBUILD_PYTHON=1 \
+  ..
+
+make
+
+cd ..
+rm -r build
+
+# ----
 
 python -m pip install . -vv
